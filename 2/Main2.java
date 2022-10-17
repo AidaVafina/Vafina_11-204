94dcfc63648b8060219288fc5fdc02614f0c503f
AidaVafina <vai2706@bk.ru>
2022-10-17 16:53:15 +0300

    upload classword

--- 2/Main2.java
+++ 2/Main2.java
@@ -2,13 +2,25 @@ import java.util.Scanner;
 
 public class Main2 {
 	public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-	int a1 = scanner.nextInt();
-	int a2 = scanner.nextInt();
-	int k = scanner.nextInt();
-	int b = a2 - a1;
-	System.out.println(a1 + (k-1) * b);
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int k = scanner.nextInt();
+		System.out.println(f(x,k));
+
+}	    
+
+	public static int f(int x,int k){
+		int i = 0;
+		int c = 1;
+		while (x > 0){
+			i += (x % k) * c;
+            c = c * 10;
+			x = x / k;
+
+		}
+		return i;
+
+	    }
 
 
-	}
 }
